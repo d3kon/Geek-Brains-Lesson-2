a4737c113bd9607ead50f29983639e878fd2807e
Dmitry <d3kon@mail.ru>
2022-12-18 16:56:44 +0800

    Исправил 13 задачу

--- Geek Brains Lesson 2/Lesson 2.cs
+++ Geek Brains Lesson 2/Lesson 2.cs
@@ -26,17 +26,11 @@ namespace Geek_Brains_Lesson_2
             string a = Console.ReadLine();
             if (a.Length >= 3)
             {
-                char[] cen_mas = new char[a.Length];
-                for (int i = 0; i < a.Length; i++)
-                {
-                    cen_mas[i] = a[i];
-                }
-                Console.WriteLine(cen_mas[2]);
+                Console.WriteLine(a[2]);
             }
             else
             {
                 Console.WriteLine("Третьего числа нет");
-                Console.ReadLine();
             }
         }
         public static void Day()
