6ed778d143acbdea2c2a1e2cd83e9839d0700754
Dmitry <d3kon@mail.ru>
2022-12-18 16:53:37 +0800

    Изменил задачу 10

--- Geek Brains Lesson 2/Lesson 2.cs
+++ Geek Brains Lesson 2/Lesson 2.cs
@@ -13,17 +13,11 @@ namespace Geek_Brains_Lesson_2
             string a = Console.ReadLine();
             if (a.Length == 3)
             {
-                char[] cen_mas = new char[a.Length];
-                for (int i = 0; i < a.Length; i++)
-                {
-                    cen_mas[i] = a[i];
-                }
-                Console.WriteLine(cen_mas[1]);
+                Console.WriteLine(a[1]);
             }
             else
             {
                 Console.WriteLine("Не трехзначное число!!");
-                Console.ReadLine();
             }
            
         }
