2bf0aae823fb87c7c41a5e803d6e35e562a31d58
Dmitry <d3kon@mail.ru>
2022-12-18 13:43:03 +0800

    + Задача 10

---
+++ Geek Brains Lesson 2/Lesson 2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    public static class Lesson2
+    {
+        public static void Center()
+        {
+            string a = Console.ReadLine();
+            if (a.Length == 3)
+            {
+                char[] cen_mas = new char[a.Length];
+                for (int i = 0; i < a.Length; i++)
+                {
+                    cen_mas[i] = a[i];
+                }
+                Console.WriteLine(cen_mas[1]);
+            }
+            else
+            {
+                Console.WriteLine("Не трехзначное число!!");
+                Console.ReadLine();
+            }
+           
+        }
+
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Geek_Brains_Lesson_2;
+
+
+Console.WriteLine("Центральное число трехзначного числа");
+Lesson2.Center();
