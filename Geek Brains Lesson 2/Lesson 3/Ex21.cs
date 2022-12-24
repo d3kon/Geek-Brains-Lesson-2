ad2dc5a4a676296c884e43af92e12579be8241fb
Dmitry <d3kon@mail.ru>
2022-12-24 18:51:11 +0800

    Задание 21

---
+++ Geek Brains Lesson 2/Lesson 3/Ex21.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2;
+    public static class Ex21
+    {
+        public static double ThirdDimDist(double A1, double A2, double A3, double B1, double B2, double B3)
+        {
+            var squareX = (A1 - B1)*(A1 - B1);
+            var squareY = (A2 - B2)*(A2 - B2);
+            var squareZ = (A3 - B3)*(A3 - B3);
+            var result = Math.Sqrt(squareX+squareY+squareZ);
+            return result;
+        }
+    }
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -2,8 +2,23 @@
 using Geek_Brains_Lesson_2;
 
 
-Console.WriteLine("Введите ");
+Console.WriteLine("Введите пятизначное число");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine(Ex19.Mirror(number));
 
 Console.WriteLine(new string('_', 50));
+
+Console.Write("A1:");
+double A1 = double.Parse(Console.ReadLine());
+Console.Write("A2:");
+double A2 = double.Parse(Console.ReadLine());
+Console.Write("A3:");
+double A3 = double.Parse(Console.ReadLine());
+Console.Write("B1:");
+double B1 = double.Parse(Console.ReadLine());
+Console.Write("B2:");
+double B2 = double.Parse(Console.ReadLine());
+Console.Write("B3:");
+double B3 = double.Parse(Console.ReadLine());
+Console.Write("Расстояние:");
+Console.WriteLine(Ex21.ThirdDimDist(A1,A2,A3,B1,B2,B3));
