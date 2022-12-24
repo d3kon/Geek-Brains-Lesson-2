f22636966770a143e6d121b3934daf7bff4add1a
Dmitry <d3kon@mail.ru>
2022-12-24 19:04:26 +0800

    Задание 23

---
+++ Geek Brains Lesson 2/Lesson 3/Ex23.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2;
+
+public static class Ex23
+{
+    public static void CubePow(int input)
+    {
+        for (int i = 1; i <= input; i++)
+        {
+            Console.Write($"{Math.Pow(i, 3)}__");
+        }
+        Console.WriteLine("");
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Geek_Brains_Lesson_2;
-
+using System.Runtime.InteropServices;
 
 Console.WriteLine("Введите пятизначное число");
 int number = int.Parse(Console.ReadLine());
@@ -21,4 +21,10 @@ double B2 = double.Parse(Console.ReadLine());
 Console.Write("B3:");
 double B3 = double.Parse(Console.ReadLine());
 Console.Write("Расстояние:");
-Console.WriteLine(Ex21.ThirdDimDist(A1,A2,A3,B1,B2,B3));
+Console.WriteLine(Ex21.ThirdDimDist(A1, A2, A3, B1, B2, B3));
+
+Console.WriteLine(new string('_', 50));
+
+Console.Write("Введите число:");
+int input = int.Parse(Console.ReadLine());
+Ex23.CubePow(input);
