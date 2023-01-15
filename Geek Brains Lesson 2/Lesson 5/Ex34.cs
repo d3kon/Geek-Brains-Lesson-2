cb7c2b21e4c8d32a27c25cebc918e779ba78ef90
Dmitry <d3kon@mail.ru>
2023-01-15 15:28:23 +0800

    Задание 34

---
+++ Geek Brains Lesson 2/Lesson 5/Ex34.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    public static class Ex34
+    {
+        public static int Even_Arr(int input)
+        {
+            var rnd = new Random();
+            double[] array = new double[input];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100,1000);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] +" ");
+            }
+
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    result++;
+                }
+            }
+            return result;
+
+        }
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -2,26 +2,8 @@
 using Geek_Brains_Lesson_2;
 using System.Runtime.InteropServices;
 
-/*Console.WriteLine(new string('_', 50));
-
-Console.WriteLine("Введите число:");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите степень:");
-int degree = int.Parse(Console.ReadLine());
-Console.WriteLine(Ex25.Degree(a,degree));
-
 Console.WriteLine(new string('_', 50));
 
-Console.WriteLine("Введите число:");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine(Ex27.Summ_Num(b));*/
-
-Console.WriteLine(new string('_', 50));
-
-Console.WriteLine("Введите числа через запятую:");
-var massiv = Console.ReadLine();
-var result = Ex29.Array(massiv);
-foreach(var item in result)
-{
-    Console.Write(item+" ");
-}
+Console.Write("Задайте длину массива: ");
+int input = int.Parse(Console.ReadLine());
+Console.WriteLine($"\nКоличество четных элементов массива: {Ex34.Even_Arr(input)}");
