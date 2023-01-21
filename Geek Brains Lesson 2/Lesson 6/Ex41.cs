639d3ab65316204bd3dda92a7f1068a4bf2a1abb
Dmitry <d3kon@mail.ru>
2023-01-21 20:41:06 +0800

    Задание 41

---
+++ Geek Brains Lesson 2/Lesson 6/Ex41.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    public static class Ex41
+    {
+        public static int Counter(string input)
+        {
+            var array = input.Split(',');
+
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (int.Parse(array[i]) > 0) result++;
+            }
+
+            return result;
+        }
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -2,23 +2,8 @@
 using Geek_Brains_Lesson_2;
 using System.Runtime.InteropServices;
 
-/*Console.WriteLine(new string('_', 50));
-
-Console.Write("Задайте длину массива: ");
-int input = int.Parse(Console.ReadLine());
-Console.WriteLine($"\nКоличество четных элементов массива: {Ex34.Even_Arr(input)}");*/
-
-
-
-/*Console.WriteLine(new string('_', 50));
-
-Console.Write("Задайте длину массива: ");
-int n = int.Parse(Console.ReadLine());
-Console.WriteLine($"\nСумма элементов на нечетных позициях: {Ex36.Even_Position(n)}");*/
-
-
 Console.WriteLine(new string('_', 50));
 
-Console.Write("Задайте длину массива: ");
-int g = int.Parse(Console.ReadLine());
-Console.WriteLine($"\nРазность максимального и минимального числа: {Ex38.Raznost_Max_Min(g)}");
+Console.Write("Введите числа через запятую, без пробелов. \nПример: 1,2,3,4 \n");
+string input = Console.ReadLine();
+Console.WriteLine($"Чисел больше нуля: {Ex41.Counter(input)}");
