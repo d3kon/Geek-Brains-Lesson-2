3553406b2d5636145ed7d820ba6a39bdbb2c373e
Dmitry <d3kon@mail.ru>
2023-01-21 21:07:54 +0800

    Задание 43

---
+++ Geek Brains Lesson 2/Lesson 6/Ex43.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    internal class Ex43
+    {
+        public static void FindCrossVector(double b1, double k1, double b2, double k2, ref double x, ref double y)
+        {
+            x = (-b2 + b1) / (-k1 + k2);
+            y = k2 * x + b2;
+        }
+    }
+}
