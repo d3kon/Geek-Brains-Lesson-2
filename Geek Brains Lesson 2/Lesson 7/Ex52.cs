479ba49c6b6105bbd8f1c5579ed451b6b697b49d
Dmitry <d3kon@mail.ru>
2023-01-23 15:30:23 +0800

    Задание 52

---
+++ Geek Brains Lesson 2/Lesson 7/Ex52.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    internal class Ex52
+    {
+        public static double[] AverageArray(int[,] array)
+        {
+
+
+            double[] result = new double[array.GetLength(1)];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    result[j] += ((double)array[i, j] / array.GetLength(0));
+                }
+
+            }
+            return result;
+
+        }
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -5,16 +5,24 @@ using System.Runtime.InteropServices;
 
 Console.WriteLine(new string('_', 50));
 
-Console.Write("Введите высоту массива 'm': ");
+Console.Write("Введите высоту массива m: ");
 int m = int.Parse(Console.ReadLine());
-Console.Write("Введите ширину массива 'n': ");
+Console.Write("Введите ширину массива n: ");
 int n = int.Parse(Console.ReadLine());
 int[,] array = Ex47.SDimArray(m, n);
 
 
-Console.Write("Введите координату 'm': ");
+Console.Write("Введите координату m: ");
 m = int.Parse(Console.ReadLine()) - 1;
-Console.Write("Введите координату 'n': ");
+Console.Write("Введите координату n: ");
 n = int.Parse(Console.ReadLine()) - 1;
 Console.Write($"Элемент найден: {Ex50.FindInSArray(array, m, n)}");
 
+
+double[] result = Ex52.AverageArray(array);
+
+Console.WriteLine("\nРезультат вычисления по столбцам:");
+foreach (var item in result)
+{
+    Console.WriteLine(item + "\t");
+}
