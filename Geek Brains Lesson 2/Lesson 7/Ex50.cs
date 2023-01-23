0d3683e14bc4f7729b79dd1a58123d2ee8fa8218
Dmitry <d3kon@mail.ru>
2023-01-23 15:25:31 +0800

    Задание 50

---
+++ Geek Brains Lesson 2/Lesson 7/Ex50.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    internal class Ex50
+    {
+        public static string FindInSArray(int[,] array, int m, int n)
+        {
+            try
+            {
+                return Convert.ToString(array[m, n]);
+            }
+
+            catch
+            {
+                return "Значение не существует";
+            }
+        }
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Geek_Brains_Lesson_2;
+using System;
 using System.Runtime.InteropServices;
 
 Console.WriteLine(new string('_', 50));
@@ -9,4 +10,11 @@ int m = int.Parse(Console.ReadLine());
 Console.Write("Введите ширину массива 'n': ");
 int n = int.Parse(Console.ReadLine());
 int[,] array = Ex47.SDimArray(m, n);
+
+
+Console.Write("Введите координату 'm': ");
+m = int.Parse(Console.ReadLine()) - 1;
+Console.Write("Введите координату 'n': ");
+n = int.Parse(Console.ReadLine()) - 1;
+Console.Write($"Элемент найден: {Ex50.FindInSArray(array, m, n)}");
 
