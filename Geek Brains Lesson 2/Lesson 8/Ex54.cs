2bd70996005f347987b25179c2f8def4523ca31c
Dmitry <d3kon@mail.ru>
2023-01-29 23:19:46 +0800

    Задание 54

---
+++ Geek Brains Lesson 2/Lesson 8/Ex54.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    internal class Ex54
+    {
+        public static void SortRows(ref int m, ref int n, int[,] array)
+        {
+            // Создание массива массивов (jagged array)
+            int[][] jArr = new int[array.GetLength(0)][];
+
+            // Инициализация подмассивов jArr
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                jArr[i] = new int[array.GetLength(1)];
+            }
+
+            // Копирование строк из источника в подмассивы
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    jArr[i][j] = array[i, j];
+                }
+            }
+
+            // Сортировка пузырьком в порядке убывания
+            for (int i = 0; i < jArr.Length; i++)
+            {
+                jArr[i] = BubbleSort(jArr[i]);
+            }
+
+            // Копирование из подмассивов в двумерный массив
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = jArr[i][j];
+                }
+            }
+
+            // Вывод массива в консоль для проверки
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+           
+        }
+
+        // Метод сортировки пузырьком в порядке убывания
+        public static int[] BubbleSort(int[] jArr)
+        {
+            int temp;
+            for (int i = 0; i < jArr.Length; i++)
+            {
+                for (int j = i + 1; j < jArr.Length; j++)
+                {
+                    if (jArr[i] < jArr[j])
+                    {
+                        temp = jArr[i];
+                        jArr[i] = jArr[j];
+                        jArr[j] = temp;
+                    }
+                }
+            }
+            return jArr;
+        }
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -5,24 +5,11 @@ using System.Runtime.InteropServices;
 
 Console.WriteLine(new string('_', 50));
 
-Console.Write("Введите высоту массива m: ");
+Console.Write("Введите высоту массива 'm': ");
 int m = int.Parse(Console.ReadLine());
-Console.Write("Введите ширину массива n: ");
+Console.Write("Введите ширину массива 'n': ");
 int n = int.Parse(Console.ReadLine());
 int[,] array = Ex47.SDimArray(m, n);
+Console.WriteLine();
 
-
-Console.Write("Введите координату m: ");
-m = int.Parse(Console.ReadLine()) - 1;
-Console.Write("Введите координату n: ");
-n = int.Parse(Console.ReadLine()) - 1;
-Console.Write($"Элемент найден: {Ex50.FindInSArray(array, m, n)}");
-
-
-double[] result = Ex52.AverageArray(array);
-
-Console.WriteLine("\nРезультат вычисления по столбцам:");
-foreach (var item in result)
-{
-    Console.WriteLine(item + "\t");
-}
+Ex54.SortRows(ref m, ref n, array);
