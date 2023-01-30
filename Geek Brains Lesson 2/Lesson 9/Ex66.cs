65bee21e8db921c7fc64dff892dac8d7f606c65a
Dmitry <d3kon@mail.ru>
2023-01-30 19:02:15 +0800

    Задание 66

---
+++ Geek Brains Lesson 2/Lesson 9/Ex66.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    internal class Ex66
+    {
+        private static int result;
+        public static int SumInRange(int m, int n)
+        {
+            if (n > m)
+            {
+                result += n;
+                n--;
+                SumInRange(m, n);
+            }
+            return result;
+        }
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -3,7 +3,7 @@ using Geek_Brains_Lesson_2;
 using System;
 using System.Runtime.InteropServices;
 
-int a= int.Parse(Console.ReadLine());
-Console.Clear();
-Ex64.SequenceNumber(a);
-Console.ReadKey();
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Ex66.SumInRange(m,n));
