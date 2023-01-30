284958ade7be6b3e5fb39ea07ecade82aecc9d99
Dmitry <d3kon@mail.ru>
2023-01-30 18:48:21 +0800

    Задание 60

---
+++ Geek Brains Lesson 2/Lesson 8/Ex60.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson_2
+{
+    internal class Ex60
+    {
+        public static void TrippleArrayGenerator()
+        {
+            Random random = new Random();
+            int[,,] array = new int[2, 2, 2];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        array[i, j, k] = random.Next(10, 100);
+                        Console.Write($"{array[i, j, k]}({i},{j},{k})");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
--- Geek Brains Lesson 2/Program.cs
+++ Geek Brains Lesson 2/Program.cs
@@ -3,12 +3,4 @@ using Geek_Brains_Lesson_2;
 using System;
 using System.Runtime.InteropServices;
 
-var data = Ex58.MatrixMultiplication();
-for (int i = 0; i < data.GetLength(0); i++)
-{
-	for (int j = 0; j < data.GetLength(1); j++)
-	{
-		Console.Write(" " + data[i,j]);
-	}
-	Console.WriteLine();
-}
+Ex60.TrippleArrayGenerator();
